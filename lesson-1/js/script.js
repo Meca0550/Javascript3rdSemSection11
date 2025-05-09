5906d2f965ae824c6bc1d0dfad22e296874decea
Meca0550 <dhakalbijay0550@gmail.com>
2025-05-09 11:32:33 -0400

    fixed few mistakes

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -25,9 +25,9 @@
 		*/
 
 		// STEP 2: Get the EM element and store as a variable, using const
-		let Element = document.querySelector('em');
+		let element = document.querySelector('em');
 		// STEP 3: Assign a click event listener to the above element, and include a reference to the function to be invoked
-		element.addEventListener('click', updatename()) ;
+		element.addEventListener('click', updatename) ;
 	
 		
 		// STEP 4: Creat a function that waits for the above click event
